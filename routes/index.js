54cf7076c15036020d1d6ad865892a38e931f496
vickiinsua <victoria.echeverria@merlin.com>
2018-05-04 17:17:37 -0400

    test

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/users/detail', function(req, res, next) {
+  res.send('hi you');
+});
+
+router.get('/test/:id', function(req,res, next){
+	res.render('test', {output : req.params.id })
+});
+
+router.post('/test/submit', function(rep, res, next){
+	var id = req.body.id;
+	res.redirect('/test/' + id)
+});
+
 module.exports = router;
